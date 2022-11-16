be62548ab61b02f3fafdd097ce969751650c31b3
timurcarstensen <tm.carstensen@icloud.com>
2022-11-16 16:45:02 +0900

    renamed pretraining to training; cleaned up pretrain training loop; refactored names for pep8 conformity; added type hints

--- src/training/pretrain.py
+++ src/training/pretrain.py
@@ -1,4 +1,5 @@
 # standard library imports
+import os
 
 # third party imports
 from torch.utils.data import DataLoader
@@ -26,6 +27,7 @@ def pretrain_model(
     criterion=nn.L1Loss(),
     lr_scheduler_step_size: int = 5,
     lr_scheduler_gamma: float = 0.1,
+    save_model: bool = True,
 ):
     # initialize the learning rate scheduler
     scheduler = lr_scheduler.StepLR(
@@ -33,9 +35,7 @@ def pretrain_model(
     )
     for epoch in range(num_epochs):
         loss_avg = []
-        model.train(True)
-
-        # start = time()
+        model.train(mode=True)
 
         for i, train_data in enumerate(train_loader):
             XI, YI = train_data
@@ -45,22 +45,16 @@ def pretrain_model(
                 y = Variable(torch.FloatTensor(YI).cuda(0), requires_grad=False)
             else:
                 x = Variable(XI)
-                print(f"x: {x}")
                 y = Variable(torch.FloatTensor(YI), requires_grad=False)
-                print(f"y: {y}")
             # Forward pass: Compute predicted y by passing x to the model
             y_pred = model(x)
 
             # Transposing tensors s.t. we can slice along the columns of the predictions and calculate the losses properly
             y_pred = y_pred.T
             y = y.T
-            # print(f"Predictions: {y_pred}")
-            # print(f"slice: {y_pred[:][:2]}")
-            # print(f"slice2: {y_pred[:][2:]}")
 
             # Compute and print loss
             running_loss = 0.0
-            print(f"length y_pred: {len(y_pred)}, batch_size: {batch_size}")
 
             # loss getting split up, 80% weight on the x, y coordinates, 20% on the width and height of the bounding box
             if len(y_pred[0]) == batch_size:
@@ -72,9 +66,9 @@ def pretrain_model(
                     loss2 = 0.2 * criterion(y_pred[:][2:], y[:][2:])
                 loss = loss1 + loss2
                 running_loss += loss1.item() + loss2.item()
-                # print(f"loss: {loss}, type: {type(loss)}, value: {loss.item()}, val: {loss.data[0]}")
+                print(f"loss: {loss}")
                 loss_avg.append(running_loss)
-                print(f"loss: {running_loss}")
+                # print(f"loss: {running_loss}")
 
                 # Zero gradients, perform a backward pass, and update the weights.
                 optimizer.zero_grad()
@@ -82,40 +76,34 @@ def pretrain_model(
                 optimizer.step()
                 scheduler.step()  # should be called after optimizer.step()
                 # torch.save(model.state_dict(), storeName)
-            # if i % 50 == 1:
-            #     with open(args["writeFile"], "a") as outF:
-            #         outF.write(
-            #             "train %s images, use %s seconds, loss %s\n"
-            #             % (
-            #                 i * batchSize,
-            #                 time() - start,
-            #                 sum(lossAver[-50:]) / len(lossAver[-50:]),
-            #             )
-            #         )
         print("%s %s\n" % (epoch, sum(loss_avg) / len(loss_avg)))
-        # with open(args["writeFile"], "a") as outF:
-        #     outF.write(
-        #         "Epoch: %s %s %s\n"
-        #         % (epoch, sum(lossAver) / len(lossAver), time() - start)
-        #     )
-        # torch.save(model.state_dict(), storeName + str(epoch))
 
-    print("Finished training")
+    if save_model:
+        print("Saving model...")
+        torch.save(
+            obj=model.state_dict(),
+            f=f"{os.getenv('MODEL_DIR')}pretrained_detection_module.pt",
+        )
+        print("Model saved.\n Finished training.")
 
     return model
 
 
 if __name__ == "__main__":
+
+    # TODO: add ArgParser
+    # TODO: add DataParallel for multi-GPU training (cf. wR2.py)
     split_directories = ["train.txt"]
 
-    batch_size = 16
+    batch_size = 8
 
     pretrain_loader = DataLoader(
-        DataLoaderPreTrain(
+        dataset=DataLoaderPreTrain(
             split_file=split_directories, img_size=(480, 480), test_mode=False
         ),
         batch_size=batch_size,
         shuffle=True,
+        num_workers=4,
     )
 
     num_classes = 4
