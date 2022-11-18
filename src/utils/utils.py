583e1a91916e2117a61c73c67f92c9b680d40926
timurcarstensen <tm.carstensen@icloud.com>
2022-11-19 00:24:22 +0900

    loading wandb api key from wandb_key_file and exporting it to the environment

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -6,3 +6,8 @@ with open(f"{os.getenv('MODEL_DIR')}../utils/provinces_and_alphabet_dict.pkl", "
     provinces_and_alphabet_dict: dict = pickle.load(f)
 
 PROVINCES, ALPHABET, ALPHABET_NUMBERS = provinces_and_alphabet_dict.values()
+
+with open(f"{os.getenv('MODEL_DIR')}../utils/wandb_key_file", "r") as f:
+    WANDB_API_KEY = f.read()
+
+os.environ["WANDB_API_KEY"] = WANDB_API_KEY
