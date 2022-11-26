a86f9b4085305b4d0c6bc62c0b5742bb84025b26
timurcarstensen <tm.carstensen@icloud.com>
2022-11-26 13:05:00 +0900

    added LearningRateMonitor callback; changed wandb entity to mtp-ai-board-game-engine

--- src/training/train.py
+++ src/training/train.py
@@ -4,7 +4,7 @@ import os
 # 3rd party imports
 import pytorch_lightning as pl
 from pytorch_lightning.loggers.wandb import WandbLogger
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 
 # local imports (i.e. our own code)
 # noinspection PyUnresolvedReferences
@@ -15,12 +15,13 @@ from src.utils.datasets import TrainDataset, TestDataset
 
 if __name__ == "__main__":
 
+    # TODO: start using the same hyperparameters as the authors for comparability (i.e. optimizer, batch_size, etc.)
     # initialise model (the train dataset is initialised with the default args)
 
     model = LitRecognitionModule(
         train_set=TrainDataset(),
         val_set=TestDataset(["val.txt"]),
-        batch_size=16,  # 16 is the default, modify according to available GPU memory
+        batch_size=128,  # 16 is the default, modify according to available GPU memory
         pretrained_model_path="pretrained_model.ckpt",  # filename of the pretrained model (in src/logs),
         # make sure this file exists in the logs folder on the machine you're running on
     )
@@ -34,28 +35,34 @@ if __name__ == "__main__":
         mode="min",
     )
 
+    # init learning rate callback
+    lr_logger = LearningRateMonitor(logging_interval="step", log_momentum=True)
+
     trainer = pl.Trainer(
         # fast_dev_run=True, # uncomment this line to run a quick test of the model
-        max_epochs=150,  # number of epochs to train for
-        callbacks=[checkpoint_callback],  # add the checkpoint callback, no need to modify
+        max_epochs=100,  # number of epochs to train for
+        callbacks=[
+            checkpoint_callback,
+            lr_logger,
+        ],  # add the checkpoint callback, no need to modify
         default_root_dir=os.getenv(
             "LOG_DIR"
         ),  # path to the logs folder, no need to modify
         strategy="ddp_find_unused_parameters_false",  # no need to modify
         log_every_n_steps=1,  # logging interval, no need to modify
-        limit_val_batches=1.0,  # validation set size, decrease for increased performance (% of the validation set)
-        limit_train_batches=1.0,  # analogous to limit_val_batches, no need to modify
+        # limit_val_batches=0.05,  # validation set size, decrease for increased performance (% of the validation set)
+        # limit_train_batches=0.05,  # analogous to limit_val_batches, no need to modify
         logger=WandbLogger(  # initialise WandbLogger, modify the group based on your current experiment
-            entity="timurcarstensen",
+            entity="mtp-ai-board-game-engine",
             project="cv-project",
-            group="training-recognition",
+            group="training",
             save_dir=os.getenv("LOG_DIR"),
             log_model=True,
         ),
         auto_scale_batch_size=True,
         auto_lr_find=True,
         accelerator="gpu",  # modify this based on the machine you're running on
-        devices=[0, 1],  # device indices for the GPUs
+        devices=[1, 2, 4, 5, 6, 7],  # device indices for the GPUs
     )
 
     trainer.fit(model=model)
