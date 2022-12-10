422ca4878c7048acfa92348a4f162b624273bf1c
Tsesterh <77850788+Tsesterh@users.noreply.github.com>
2022-12-11 01:11:13 +0900

    Added method that cuts out the box from an image

--- src/utilities/datasets.py
+++ src/utilities/datasets.py
@@ -12,11 +12,11 @@ import numpy as np
 
 class PretrainDataset(Dataset):
     def __init__(
-        self,
-        split_file: List[str] | str = ["train.txt"],
-        img_size: Tuple[int, int] = (480, 480),
-        is_transform: bool = None,
-        test_mode: bool = False,
+            self,
+            split_file: List[str] | str = ["train.txt"],
+            img_size: Tuple[int, int] = (480, 480),
+            is_transform: bool = None,
+            test_mode: bool = False,
     ):
         """
         Initialises the Pretraining dataset
@@ -78,11 +78,11 @@ class PretrainDataset(Dataset):
 
 class TrainDataset(Dataset):
     def __init__(
-        self,
-        split_file: List[str] | str = ["train.txt"],
-        img_size: Tuple = (480, 480),
-        is_transform: bool = None,
-        test_mode: bool = False,
+            self,
+            split_file: List[str] | str = ["train.txt"],
+            img_size: Tuple = (480, 480),
+            is_transform: bool = None,
+            test_mode: bool = False,
     ):
         """
         Initialises the Train Dataset
@@ -133,14 +133,31 @@ class TrainDataset(Dataset):
 
         return image, lp_box, lp_char_cls, img_name
 
+    def get_boxpart_from_img(self, index):
+        img_name = self.img_paths[index]
+        img = cv.imread(img_name)
+        iname = img_name.rsplit("/", 1)[-1].rsplit(".", 1)[0].split("-")
+        [left_up, right_down] = [
+            [int(eel) for eel in el.split("&")] for el in iname[2].split("_")
+        ]
+        print(left_up)
+        print(right_down)
+
+        box_img = img[left_up[1]:right_down[1], left_up[0]:right_down[0]]
+        box_img = cv.resize(box_img, self.img_size)  # TODO: this may be changed
+        box_img = np.transpose(box_img, (2, 0, 1))
+        box_img = box_img.astype("float32")
+        box_img /= 255.0
+        return box_img
+
 
 class TestDataset(Dataset):
     def __init__(
-        self,
-        split_file: List[str] | str = ["test.txt"],
-        img_size: Tuple[int, int] = (480, 480),
-        is_transform: bool = None,
-        test_mode: bool = False,
+            self,
+            split_file: List[str] | str = ["test.txt"],
+            img_size: Tuple[int, int] = (480, 480),
+            is_transform: bool = None,
+            test_mode: bool = False,
     ):
         """
         Initialises the Test Dataset
@@ -178,3 +195,20 @@ class TestDataset(Dataset):
         labels = img_name.split("/")[-1].split(".")[0].split("-")[-3]
 
         return resized_image, labels, img_name
+
+    def get_boxpart_from_img(self, index):
+        img_name = self.img_paths[index]
+        img = cv.imread(img_name)
+        iname = img_name.rsplit("/", 1)[-1].rsplit(".", 1)[0].split("-")
+        [left_up, right_down] = [
+            [int(eel) for eel in el.split("&")] for el in iname[2].split("_")
+        ]
+        print(left_up)
+        print(right_down)
+
+        box_img = img[left_up[1]:right_down[1], left_up[0]:right_down[0]]
+        box_img = cv.resize(box_img, self.img_size)  # TODO: this may be changed
+        box_img = np.transpose(box_img, (2, 0, 1))
+        box_img = box_img.astype("float32")
+        box_img /= 255.0
+        return box_img
