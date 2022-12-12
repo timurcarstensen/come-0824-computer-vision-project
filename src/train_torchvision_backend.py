08e6ec1f8401117d383e53ab95a5bb586969eaf9
timurcarstensen <tm.carstensen@icloud.com>
2022-12-12 14:29:23 +0900

    added training file for the model with a torchvision backend

--- src/train_torchvision_backend.py
+++ src/train_torchvision_backend.py
@@ -1,6 +1,3 @@
-# standard library imports
-import os
-
 # 3rd party imports
 import pytorch_lightning as pl
 from pytorch_lightning.loggers.wandb import WandbLogger
@@ -12,40 +9,46 @@ import utilities.setup_utils
 from src.modules.torchvision_backend.recognition_module import RecognitionModule
 
 
-# checkpoint_callback = ModelCheckpoint(
-#     monitor="pretrain_loss",
-#     filename="detection-{epoch:02d}-{pretrain_loss:.2f}",
-#     save_top_k=3,
-#     mode="min",
-# )
-# 2. learning rate monitor callback
-# lr_logger = LearningRateMonitor(logging_interval="step", log_momentum=True)
+if __name__ == "__main__":
+    checkpoint_callback = ModelCheckpoint(
+        monitor="pretrain_loss",
+        filename="detection-{epoch:02d}-{pretrain_loss:.2f}",
+        save_top_k=3,
+        mode="min",
+    )
+    # 2. learning rate monitor callback
+    lr_logger = LearningRateMonitor(logging_interval="step", log_momentum=True)
 
-# defining the model
-detection_model = RecognitionModule(
-    batch_size=2, pretrained_model_path="resnet_backend.ckpt"
-)
+    # defining the model
+    recognition_module = RecognitionModule(
+        batch_size=16,
+        pretrained_model_path="resnet_backend.ckpt",
+        crop_size=(112, 112),
+        fine_tuning=True,
+    )
 
-# print(detection_model)
+    # print(detection_model)
 
-trainer = pl.Trainer(
-    fast_dev_run=True,
-    max_epochs=100,
-    # callbacks=[checkpoint_callback, lr_logger],
-    # limit_train_batches=0.05,
-    # limit_test_batches=0.05,
-    # limit_val_batches=0.05,
-    log_every_n_steps=1,
-    logger=WandbLogger(
-        entity="mtp-ai-board-game-engine",
-        project="cv-project",
-        group="pretraining-resnet-backbone",
-        log_model="all",
-    ),
-    # auto_scale_batch_size=True,
-    # auto_lr_find=True,
-    # accelerator="gpu",
-    # devices=[0, 1],
-)
+    trainer = pl.Trainer(
+        # fast_dev_run=True,
+        max_epochs=100,
+        precision=16,
+        callbacks=[checkpoint_callback, lr_logger],
+        # limit_train_batches=0.05,
+        # limit_test_batches=0.05,
+        # limit_val_batches=0.05,
+        log_every_n_steps=1,
+        logger=WandbLogger(
+            entity="mtp-ai-board-game-engine",
+            project="cv-project",
+            name="fine-tuning-crop-size-112",
+            group="training-resnet-backbone",
+            log_model="all",
+        ),
+        # auto_scale_batch_size=True,
+        # auto_lr_find=True,
+        accelerator="gpu",
+        devices=[0, 1],
+    )
 
-trainer.fit(model=detection_model)
+    trainer.fit(model=recognition_module)
