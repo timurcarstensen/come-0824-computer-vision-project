0e6256c0e0ae50e8599c8819f2d38b8157532ef0
timurcarstensen <tm.carstensen@icloud.com>
2022-12-12 14:28:22 +0900

    updated crop_module.py CropModule to inherit from pl.LightningModule and assigned all tensors to self.device; docstring; formatting

--- src/modules/torchvision_backend/crop_module.py
+++ src/modules/torchvision_backend/crop_module.py
@@ -1,12 +1,19 @@
-import torch.nn as nn
+# 3rd party imports
 import torch
 from torchvision.transforms import Resize
 from torchvision.ops import box_convert
-import numpy as np
+import pytorch_lightning as pl
 
 
-class CropModule(nn.Module):
+class CropModule(pl.LightningModule):
+    """
+    Crops the images in the batch according to the bounding boxes provided by the DetectionModule and resizes
+    to the specified size.
+    """
+
     def __init__(self, size):
+        """Initialises the CropModule"""
+
         super(CropModule, self).__init__()
 
         self.resize = Resize(size)
@@ -18,11 +25,13 @@ class CropModule(nn.Module):
 
         for idx, elem in enumerate(box):
             elem = elem.flatten()
-            elem = elem * torch.tensor([480, 480, 480, 480])
+            elem = elem * torch.tensor([480, 480, 480, 480], device=self.device)
 
             elem = (
                 box_convert(
-                    torch.tensor([[elem[0], elem[1], elem[2], elem[3]]]),
+                    torch.tensor(
+                        [[elem[0], elem[1], elem[2], elem[3]]], device=self.device
+                    ),
                     in_fmt="cxcywh",
                     out_fmt="xyxy",
                 )
